4d06ee3740ab40bf491e4fad908deb5b3456878a
Damir Kolobaric <dkolobaric@gmail.com>
2022-09-24 17:50:18 +0200

    Header

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import Container from 'react-bootstrap/Container'
+import Nav from 'react-bootstrap/Nav'
+import Navbar from 'react-bootstrap/Navbar'
+import NavDropdown from 'react-bootstrap/NavDropdown'
+import Image from 'react-bootstrap/Image'
+
+import avatar from '../assets/avatar.png'
+
+function Header() {
+  return (
+    <Navbar bg='light' expand='lg'>
+      <Container>
+        <Navbar.Brand href='#home'>React-Bootstrap</Navbar.Brand>
+        <Navbar.Toggle aria-controls='basic-navbar-nav' />
+        <Navbar.Collapse id='basic-navbar-nav'>
+          <Nav className='me-auto'>
+            <Nav.Link href='#home'>Home</Nav.Link>
+            <Nav.Link href='#link'>Link</Nav.Link>
+          </Nav>
+          <Nav>
+            <Nav.Link eventKey={2} href='#memes'>
+              Dank memes
+            </Nav.Link>
+            <Image className='avatar' src={avatar} />
+            <NavDropdown title='Dropdown' id='basic-nav-dropdown'>
+              <NavDropdown.Item href='#action/3.1'>Action</NavDropdown.Item>
+              <NavDropdown.Item href='#action/3.2'>
+                Another action
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  )
+}
+
+export default Header
