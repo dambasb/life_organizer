6d01eead8aea62746b47c670b0b7afe2474a5f32
Damir Kolobaric <dkolobaric@gmail.com>
2022-12-03 19:54:42 +0100

    removing unnecessary files

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,21 @@
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './App.css'
 import Header from './components/Header'
+import LoginScreen from './screens/LoginScreen'
 import ToDoScreen from './screens/ToDoScreen'
 
 function App() {
   return (
-    <>
+    <Router>
       <Header />
-      <ToDoScreen />
-    </>
+      <main>
+        <Routes>
+          <Route path='/' component={ToDoScreen} exact />
+          <Route path='/login' component={LoginScreen} />
+        </Routes>
+      </main>
+    </Router>
   )
 }
 
